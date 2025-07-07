5b3fa01a631a748c12e5dd523ef878a2a0121cb1
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-07-07 16:19:44 +0530

    db: update models
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- backend/models.go
+++ backend/models.go
@@ -5,7 +5,7 @@ import (
 )
 
 type user struct {
-	ID        string    `json:"id"`
+	ID        string    `json:"id" gorm:"primaryKey"`
 	Email     string    `json:"email"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
@@ -13,8 +13,10 @@ type user struct {
 }
 
 type url struct {
-	ShortUrl  string    `json:"short_url"`
+	ShortUrl  string    `json:"short_url" gorm:"primaryKey"`
 	LongUrl   string    `json:"long_url"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+	UserID    string    `json:"user_id"`
+	User      user      `gorm:"foreignKey:UserID"`
 }
